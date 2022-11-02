d67b19276d9cc5e44e312b9e7b01c63e85f76cd7
Lutz Gross <6036995+LutzGross@users.noreply.github.com>
2022-11-02 16:26:38 +1000

    initial checkin

--- Notebooks/GeophysicalImaging/resources/Tools/build.py
+++ Notebooks/GeophysicalImaging/resources/Tools/build.py
@@ -2,13 +2,15 @@
 import json, os 
 import shutil
 
-COPY1=['01_PythonBasics.ipynb', '02_Arrays.ipynb', 'Data/gravity_tasmania.csv', 'Data/marmousi.bin' ]
-COPY=[ '03_Gravity.ipynb', '04_Magnetic.ipynb', '05_Magnetotellurics.ipynb', '06_WavesFrequency.ipynb', 'Data/IMAGE_CS1.png', 'Data/IMAGE_Dyke.jpg', 
-        'Data/MTImage1.png', 'esys-escript.pdf', 'Data/SonicImage1.png']
+COPY=[ '00_Intro.ipynb', '01_PythonBasics.ipynb', '02_Arrays.ipynb', '11_FEM.ipynb', '21_Gravity.ipynb', '22_Magnetic.ipynb',
+   '22_Magnetotellurics.ipynb', '23_WavesFrequency.ipynb', '26_WavesTime.ipynb', '41_InversionAsLSQ.ipynb', '42_inversionGrav.ipynb',
+   '43_inversionMT.ipynb', '44_inversionJoint.ipynb', 'mytools.py' ]
+DCOPY = [ 'IMAGE_CS1.png', 'IMAGE_Dyke.jpg', 'IMAGE_Indicator_function.png', 'IMAGE_filter_function.png', 'IMAGE_fitting_grid.png',
+  'Image_minima.png', 'MTImage1.png', 'SonicImage1.png', 'gravity_tasmania.csv', 'marmousi.bin' ]
 
 
 SRCDIR='../Notebooks'
-RELEASEDIR='../material/Notebooks'
+RELEASEDIR='../../material/Notebooks'
 
 ANSWER= "SOLUTION"
 
@@ -25,7 +27,7 @@ def rmAnswers(src, dst):
     json.dump(text, open(dst, 'w'), indent=3) 
 
 
-for fn in COPY:
+for fn in COPY + [ os.path.join("Data", f) for f in DCOPY]:
     frm=os.path.join(SRCDIR, fn)
     to=os.path.join(RELEASEDIR, fn)
     if os.path.splitext(fn)[1] == ".ipynb":
